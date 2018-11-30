54d5617ca3f942dde1f75f7b5d31f4ff9a4e0aee
rh-chan <rh.chan@mail.utoronto.ca>
2018-11-30 14:16:38 -0500

    InputFile class with file splitting

--- RyanChan_CodingChallenge.py
+++ RyanChan_CodingChallenge.py
@@ -11,20 +11,50 @@ Assumptions
 
 """
 from collections import deque
+import sys
 
 
 class InputFile:
+    PARTITION_SIZE = 1000
+    PARTITION_NAME = "partition{0}"
+
     def __init__(self, filename):
         self.filename = filename
         self.partitions = deque()
+
+    @staticmethod
+    def read_lines(lines):
+        """
+        Read each block of lines keeping track of unique words
+        :return: list of sorted unique words
+        """
+        words = set()
+        for line in lines:
+            words.update(line.split())
+        return sorted(list(words))
+
+    def write_sorted_to_file(self, words, partition_number):
+        """
+        Writes the sorted, unique words into an output file
+        """
+        partition_name = self.PARTITION_NAME.format(partition_number)
+        file = open(partition_name, 'w')
+        for word in words:
+            file.write(word + "\n")
+        file.close()
+        self.partitions.append(partition_name)
 
     def split_file(self):
         """
         Split input file into partitions
         """
-        pass
+        file, partition = open(self.filename, 'r'), 0
+        lines = file.readlines(self.PARTITION_SIZE)
+        while lines:
+            self.write_sorted_to_file(self.read_lines(lines), partition)
+            lines, partition = file.readlines(self.PARTITION_SIZE), partition + 1
 
-    def partitions(self):
+    def get_partitions(self):
         """
         :return: a queue for the name of the partitions
         """
@@ -47,4 +77,11 @@ def write_merged_to_output(merged_name, output_name):
 
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "small_input.txt"
+
+    file = InputFile(filename)
+    file.split_file()
+    queue = file.get_partitions()
